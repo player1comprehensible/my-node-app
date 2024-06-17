65ce6c1501ca5b440f3b18819d95a1d01fd38bf2
player1comprehensible <jesperanca@ipvc.pt>
2024-06-17 10:38:38 +0100

    Adicionar frase

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Olá sou o José. Antes só tinha escrito até 'José.'");
+  res.send("Olá sou o José Demo to até 'José.'");
 });
 
 app.listen(port, () => {
